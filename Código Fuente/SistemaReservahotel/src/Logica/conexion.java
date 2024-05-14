2f20036c8db039f4569601af8fba3073c64d4f05
Pablo <pablonietoperez369@gmail.com>
2024-05-14 01:08:12 -0500

    Sistema de reserva completo

---
+++ Código Fuente/SistemaReservahotel/src/Logica/conexion.java
@@ -0,0 +1,36 @@
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class conexion {
+    public String db="basereserva";
+    public String url="jdbc:mysql://127.0.0.1/" +db;
+    public String user="root";
+    public String pass="root";
+
+    public conexion() {
+    }
+    
+    public Connection conectar(){
+        Connection link=null;
+        
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link=DriverManager.getConnection(this.url, this.user, this.pass);
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            
+        }
+        
+        return link;
+    }
+    
+   
+    
+    
+}
